0d0e0c993b67b17b55a1acbf2eebef9d90de42c2
KT <koczkatamas@gmail.com>
2020-09-01 03:02:18 +0200

    Fix Python runner

--- ProjectTest/OneLang/Python/main.py
+++ ProjectTest/OneLang/Python/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.dirname(__file__) + "/onepkg")
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/onepkg")
 
 from OneLang.Generator.CsharpGenerator import CsharpGenerator
 from OneLang.Test.SelfTestRunner import SelfTestRunner
